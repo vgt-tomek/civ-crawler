b531815941702e16ce2dccf9d8b6ea860c0d112e
VGT <tomek@vgtworld.pl>
2014-05-09 23:23:05 +0200

    Add searching thread ids and store results in database in scheduled task.

--- civ-forum-crawler/src/main/java/pl/vgtworld/civcrawler/crawler/Crawler.java
+++ civ-forum-crawler/src/main/java/pl/vgtworld/civcrawler/crawler/Crawler.java
@@ -1,12 +1,21 @@
 package pl.vgtworld.civcrawler.crawler;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.ejb.Schedule;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
+import pl.vgtworld.civcrawler.entities.Post;
+import pl.vgtworld.civcrawler.entities.Thread;
+import pl.vgtworld.civcrawler.parsers.ParseException;
 import pl.vgtworld.civcrawler.parsers.PostDto;
+import pl.vgtworld.civcrawler.parsers.ThreadDto;
+import pl.vgtworld.civcrawler.parsers.ThreadParser;
 import pl.vgtworld.civcrawler.parsers.TodayPostsParser;
+import pl.vgtworld.civcrawler.services.PostsService;
+import pl.vgtworld.civcrawler.services.ThreadsService;
 
 @Stateless
 public class Crawler {
@@ -15,13 +24,44 @@ public class Crawler {
 	
 	private static final String TODAY_POSTS_URL = HOST + "search.php?action=viewtoday";
 	
+	private static final String THREAD_URL = HOST + "misc.php?action=gotomsg&MsgID=%1$d";
+	
+	@Inject
+	PostsService postsService;
+	
+	@Inject
+	ThreadsService threadsService;
+	
 	@Schedule(persistent=false, second="0", minute="*/5", hour="*")
-	public void searchNewPosts() throws IOException {
+	public void searchNewPosts() throws IOException, ParseException {
 		String todayPostsPage = HttpUtils.downloadUrl(TODAY_POSTS_URL);
 		TodayPostsParser todayParser = new TodayPostsParser();
+		ThreadParser threadParser = new ThreadParser();
 		
 		PostDto[] posts = todayParser.parse(todayPostsPage);
 		
-		//TODO Find threadId for each post and store result in database.
+		for (PostDto postDto : posts) {
+			Post postEntity = postsService.findById(postDto.getMessageId());
+			
+			if (postEntity == null) {
+				String threadUrl = String.format(THREAD_URL, postDto.getMessageId());
+				String threadPage = HttpUtils.downloadUrl(threadUrl);
+				ThreadDto threadDto = threadParser.parse(threadPage);
+				
+				Thread thread = threadsService.findById(threadDto.getThreadId());
+				if (thread == null) {
+					thread = new Thread();
+					thread.setId(threadDto.getThreadId());
+					thread.setTitle("");
+				}
+				Post post = new Post();
+				post.setId(postDto.getMessageId());
+				post.setThread(thread);
+				post.setPage(threadDto.getPage());
+				post.setCreatedAt(new Date());
+				postsService.add(post);
+			}
+		}
 	}
+	
 }
